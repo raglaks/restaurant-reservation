343bd86a5594224c038383ed81a5a99d305603f8
Edgar Burelo <edgar.burelo@gmail.com>
2018-10-27 11:42:19 -0500

    fs update

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const PORT = 3000;
 let mainArr = [];
 let waitArr = [];
 
+fs.readFile("mainArr.txt", "utf8", function (error, data) {
+    if (error) {
+        return console.log(error);
+    }
+    if (data) {
+        console.log(data);
+    } else {
+        console.log("no hay data");
+    }
+    //var dataArr = data.split(",");
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
