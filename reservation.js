ed05096fc7f50d179720e19a036793c550236653
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-27 11:40:00 -0500

    reservation constructor function added in reservation.js and exported to server file for use

---
+++ reservation.js
@@ -0,0 +1,13 @@
+const Reservation = function (name, phone, mail, ID) {
+
+    this.name = name,
+
+    this.phone = phone,
+
+    this.mail = mail,
+
+    this.ID = ID
+
+}
+
+module.exports = Reservation;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const Reservation = require("./reservation.js");
 
 const app = express();
 const PORT = 3000;
